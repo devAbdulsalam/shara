a8d3f1a38fdfd280e1a4131f9bbe874108c0c85b
my-amtech <ammuftau74@gmail.com>
2022-10-29 12:48:52 +0100

    another commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import Index from './components/Index';
+// import Despose from './components/Despose';
+import WasteInfo from './components/WasteInfo';
+// import Index from './components/Index';
 function App() {
   return (
     <div className="App">
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Index></Index>
+      {/* <Index /> */}
+      {/* <Despose /> */}
+      <WasteInfo />
     </div>
   );
 }

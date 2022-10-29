dc30f11414a85740d93f55d0ec14035a22ef0609
my-amtech <ammuftau74@gmail.com>
2022-10-29 14:48:51 +0100

    First commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,16 +22,16 @@ const Index = () => {
                 <div  className="mr-auto place-self-center lg:col-span-7 md:text-left text-center"  >
                     <a href="./signin.html" className="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100 hover:text-[#407BFF]" >
                         <div className="py-3">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            viewBox="0 0 512 512"
-                            fill="#407BFF"
-                        >
-                            <path
-                            d="M352 96h64c17.7 0 32 14.3 32 32V384c0 17.7-14.3 32-32 32H352c-17.7 0-32 14.3-32 32s14.3 32 32 32h64c53 0 96-43 96-96V128c0-53-43-96-96-96H352c-17.7 0-32 14.3-32 32s14.3 32 32 32zm-7.5 177.4c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22v72H32c-17.7 0-32 14.3-32 32v64c0 17.7 14.3 32 32 32H160v72c0 9.6 5.7 18.2 14.5 22s19 2 26-4.6l144-136z"
-                            />
-                        </svg>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6"
+                                viewBox="0 0 512 512"
+                                fill="#407BFF"
+                            >
+                                <path
+                                d="M352 96h64c17.7 0 32 14.3 32 32V384c0 17.7-14.3 32-32 32H352c-17.7 0-32 14.3-32 32s14.3 32 32 32h64c53 0 96-43 96-96V128c0-53-43-96-96-96H352c-17.7 0-32 14.3-32 32s14.3 32 32 32zm-7.5 177.4c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22v72H32c-17.7 0-32 14.3-32 32v64c0 17.7 14.3 32 32 32H160v72c0 9.6 5.7 18.2 14.5 22s19 2 26-4.6l144-136z"
+                                />
+                            </svg>
                         </div>
                         <h1 className="py-3 w-5/6 text-center text-gray-600 font-bold hover:text-[#407BFF]">
                         Sign In
@@ -40,8 +40,7 @@ const Index = () => {
                     <div className="mt-4 flex items-center justify-between">
                         <span className="border-b w-1/5 lg:w-1/4"></span>
                         <a href="./login.html" className="text-xs text-center text-gray-500 uppercase"
-                        >or login</a
-                        >
+                        >or login</a>
                         <span className="border-b w-1/5 lg:w-1/4"></span>
                     </div>
                     <a href="./login.html" className="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100 hover:text-[#407BFF]"    >
@@ -60,7 +59,7 @@ const Index = () => {
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
                         maxime.
                     </p>
-                </div>
+                </div>                
             </div>
         </section>
     </div>
